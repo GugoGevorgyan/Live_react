bd0711f5235b355ad1e79ea8c27e3a9c28a31988
Gurgen <gugo1519@gmail.com>
2020-08-19 12:16:24 +0400

    first commit

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Redirect, Route} from 'react-router-dom';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+export default class createRoutes extends Component {
+
+    render() {
+        return (
+            <Router>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+                <Route path="/dashboard" component={Dashboard}/>
+            </Router>
+        )
+    }
+
+};
