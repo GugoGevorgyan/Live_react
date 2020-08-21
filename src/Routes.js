2adf101f73a0ea5a9c11ebe2d58c3910c8603293
Gugo <gugo1519@gmail.com>
2020-08-21 16:39:29 +0300

    prod kisat

--- src/Routes.js
+++ src/Routes.js
@@ -3,10 +3,13 @@ import {BrowserRouter as Router, Redirect, Route} from 'react-router-dom';
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Product from "./components/product";
+
 export default class createRoutes extends Component {
     render() {
         return (
             <Router>
+                <Route path='/product' component={Product}/>
                 <Route path="/login" component={Login}/>
                 <Route path="/register" component={Register}/>
                 <Route path="/dashboard" component={Dashboard}/>
