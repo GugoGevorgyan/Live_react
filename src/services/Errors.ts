371d43c27f16db7582882abca64fe6c136f342b0
Gugo <gugo1519@gmail.com>
2020-08-27 12:49:46 +0300

    err

---
+++ src/services/Errors.ts
@@ -0,0 +1,16 @@
+class Errors{
+errors = {};
+getErrors(){
+return this.errors;
+}
+setErrors(response){
+this.errors = response.data.errors;
+}
+getKey(key:string){
+return(this.errors[key] !== undefined) ? this.errors[key] : null;
+}
+resert (){
+    this.errors = {};
+}
+}
+export default new Errors();
